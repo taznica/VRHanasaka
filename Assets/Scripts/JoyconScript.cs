8b45f55611a1611b96fdc9a5495e14de037831a0
ttsutchi <tailegog@gmail.com>
2019-03-09 18:46:15 +0900

    [Add] add joycon files and succeeded using joycon

---
+++ Assets/Scripts/JoyconScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyconScript : MonoBehaviour {
+
+	private List<Joycon> joycons;
+	private Joycon joycon;
+
+	// Use this for initialization
+	void Start () {
+		joycons = JoyconManager.Instance.j;
+
+		// joycon = joycons.Find(con => con.isLeft);
+		joycon = joycons.Find(con => !con.isLeft);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(joycons == null || joycons.Count <= 0){
+			Debug.Log("null");
+			return;
+		}
+
+		GetAngles();
+	}
+
+	private void GetAngles(){
+		var orientation = joycon.GetVector().eulerAngles;
+		var angles = transform.localEulerAngles;
+
+		angles.x = orientation.x;
+		angles.y = orientation.z;
+		angles.z = -orientation.y;
+
+		transform.localEulerAngles = angles;
+	}
+}
